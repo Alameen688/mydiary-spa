6f6a22c340df8935d9a56b8914d954a18787f406
Alameen688 <ogundiran12@gmail.com>
2019-06-20 14:01:40 +0100

    ch(prop-type): add proptypes to protected an guest route component - update react and react-dom - add proptypes

--- src/component/HOC/GuestRoute.jsx
+++ src/component/HOC/GuestRoute.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { Route, Redirect } from 'react-router-dom';
+import PropTypes from 'prop-types';
 import HeaderComponent from '../container/Header';
 
 export const GuestRoute = ({ isLoggedIn, component: Component, ...rest }) => (
@@ -16,5 +17,11 @@ export const GuestRoute = ({ isLoggedIn, component: Component, ...rest }) => (
 const mapStateToProps = state => ({
   isLoggedIn: state.login.user
 });
+
+GuestRoute.propTypes = {
+  isLoggedIn: PropTypes.bool.isRequired,
+  component: PropTypes.func.isRequired,
+  location: PropTypes.object.isRequired,
+};
 
 export default connect(mapStateToProps)(GuestRoute);
--- src/component/HOC/ProtectedRoute.jsx
+++ src/component/HOC/ProtectedRoute.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { Route, Redirect } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
 export const ProtectedRoute = ({ isLoggedIn, component: Component, ...rest }) => (
   <Route
@@ -15,5 +16,11 @@ export const ProtectedRoute = ({ isLoggedIn, component: Component, ...rest }) =>
 const mapStateToProps = state => ({
   isLoggedIn: state.login.user
 });
+
+ProtectedRoute.propTypes = {
+  isLoggedIn: PropTypes.bool.isRequired,
+  component: PropTypes.func.isRequired,
+  location: PropTypes.object.isRequired,
+};
 
 export default connect(mapStateToProps)(ProtectedRoute);
